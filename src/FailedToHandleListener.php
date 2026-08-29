da0f3ad325450749c4042b0f1fb102133fdbd016
Verdient <fagui.zhu@foxmail.com>
2026-08-29 14:29:17 +0800

    feat: 移除对于忽略异常日志的豁免

--- src/FailedToHandleListener.php
+++ src/FailedToHandleListener.php
@@ -11,7 +11,6 @@ use Verdient\Hyperf3\Job\AbstractJob;
 use Verdient\Hyperf3\Task\Logger\ConsumeLoggerInterface;
 use Verdient\Hyperf3\Task\LoggerManager;
 use Verdient\Job\Event\FailedToHandle;
-use Verdient\Job\Exception\IgnoreException;
 
 /**
  * 处理失败事件监听器
@@ -64,10 +63,6 @@ class FailedToHandleListener implements ListenerInterface
     #[Override]
     public function process(object $event): void
     {
-        if ($event->throwable instanceof IgnoreException) {
-            return;
-        }
-
         $cost = $event->endAt - $event->startAt;
 
         $this->getLogger($event)->info(sprintf('任务 %s 处理失败，耗时 %.4f 秒。', $event->job::class, $cost));
